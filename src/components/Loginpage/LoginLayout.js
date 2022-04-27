06bc27c9290dfa710e14308dff60aa79772f46f6
MANIDEEP GODAVARTHY <manideepgodavarthy@gmail.com>
2022-04-28 01:15:15 +0530

    changed home page

--- src/components/Loginpage/LoginLayout.js
+++ src/components/Loginpage/LoginLayout.js
@@ -15,15 +15,21 @@ const LoginLayout = (props) => {
         <div>
             <Row className="fullHeight">
                 <Col sm={7} className="leftCol">
+                    
+                <div className="centerDiv">
+                    <div>
+                        This page is for the Beetle Nut Institution for Pasadena city. Here we have accounts for each branch which are single/multiple pincodes. If a non user wants to notify any of the pincode he/she can fill the form in the search button below and notify the branches covering the pincodes. Then the branch manager reads them and will take appropriate action. If you are a branch manager you can login with your credentials from the login button on the right side and read the notifications. User can also create a new branch for the same Institution in the city by selecting create branch on the right side.
+                    </div>
                             <Searchbox />
+                            </div>
                 </Col>
                 <Col className="rightCol" >
                     <div className="centerDiv">
                 <div className="subTitle">
-                    <span >Log in to Scavengers</span>
+                    <span >Beetle Nut Institution for Pasadena city.</span>
                     
                     <NewLogin setitem = {props.setitem} />
-                    <h6>Or</h6>
+                    <h6> - Or -</h6>
                     <AddBranch />
                 </div>
             </div>
