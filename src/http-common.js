c33e21473296e383b609b57879525a4eff424a72
Manideep Godavarthy <61969497+ManideepPspk@users.noreply.github.com>
2022-04-28 22:07:59 +0530

    Update http-common.js

--- src/http-common.js
+++ src/http-common.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "http://localhost:4500",
-  //baseURL: "https://warm-island-56693.herokuapp.com",
+  //baseURL: "http://localhost:4500",
+  baseURL: "https://warm-island-56693.herokuapp.com",
   headers: {
     "Content-type": "application/json"
   }
